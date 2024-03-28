a6e0c6e1319b5ed6ec2c469c69f545494099ff47
Nassim <101125927+Nassim-H@users.noreply.github.com>
2024-03-28 11:04:22 +0100

    Timeline inversée

--- app/parcours/page.tsx
+++ app/parcours/page.tsx
@@ -43,49 +43,52 @@ const Experience = () => {
          </div>
         
         <VerticalTimeline>
-            <VerticalTimelineElement
-                className="vertical-timeline-element--work"
-                contentStyle={{ background: 'rgb(126, 200, 227)' }}
-                contentArrowStyle={{ borderRight: '7px solid  rgb(126, 200, 227)' }}
-                date="2016 - 2018"
-                iconStyle={{ background: 'rgb(162, 209, 73)', color: '#fff' }}
-                icon={<WorkIcon />}
-                visible={true}
-                        >
-                <h3 className="vertical-timeline-element-title text-green-900 text-xl">Auto apprentissage de l&apos;informatique</h3>
-                <h4 className="vertical-timeline-element-subtitle ">Les débuts en autodidacte</h4>
-                <p>
-                Je découvre l&apos;informatique et <b>l&apos;auto apprentissage</b> sur internet durant mes années de <b>collège et de lycée</b>. J&apos;apprends notamment sur <em>le site du 0</em> qui devient ensuite <a href="https://openclassrooms.com/fr/" target="_blank" className="text-blue-700">openclassroom</a>.
-                </p>
-            </VerticalTimelineElement>
+          <VerticalTimelineElement
+            className="vertical-timeline-element--education"
+            date="Aujourd'hui"
+            contentStyle={{ background: 'rgb(126, 200, 227)' }}
+            contentArrowStyle={{ borderRight: '7px solid  rgb(126, 200, 227)' }}
+            iconStyle={{ background: 'rgb(162, 209, 73)', color: '#fff' }}
+            visible={true}
+
+          >
+            <h3 className="vertical-timeline-element-title text-green-900 text-xl">Candidature à la Licence 3 en éducation</h3>
+            <h4 className="vertical-timeline-element-subtitle">Licence éducation à distance</h4>
+            <p>
+              Suite à mon <b>parcours formateur</b> dans l&apos;enseignement, je souhaite continuer dans cette voie. Je postule donc pour une <b>licence 3 en éducation</b> à distance. J&apos;espère ainsi continuer à <b>m&apos;investir</b> dans l&apos;éducation et à aider les enfants à grandir. Au-délà de ma passion pour l&apos;enseignement, c&apos;est également les méthodes d&apos;apprentissage qui m&apos;intéressent. Curieux et ambitieux, j&apos;aime <b>comprendre</b> comment la société évolue afin d&apos;être un acteur de son amélioration.
+            </p>
+        </VerticalTimelineElement>
+
+
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="2024"
+          iconStyle={{ background: 'rgb(162, 209, 73)', color: '#fff' }}
+          visible={true}
+
+        >
+          <h3 className="vertical-timeline-element-title text-green-900 text-xl">Diplôme d&apos;Animateur Fédéral</h3>
+          <h4 className="vertical-timeline-element-subtitle">Karaté</h4>
+          <p>
+            A côté de mes études, je continue après <b>10 ans</b> de pratique, de m&apos;investir dans le <b>karaté</b>. Je suis en train de passer mon <b><a href="https://www.ffkarate.fr/espace-licencies/formations/diplome-animateur-federal-daf/" target="_blank" className="text-blue-700" >diplôme d&apos;animateur fédéral</a></b> tout en <b>enseignant</b> avec le professeur dans mon club. 
+          </p>
+      </VerticalTimelineElement>
+
       <VerticalTimelineElement
         className="vertical-timeline-element--work"
-        date="2020 - 2021"
-        iconStyle={{ background: 'rgb(126, 200, 227)', color: '#fff' }}
+        date="2023 - 2024"
+        iconStyle={{ background: 'rgb(162, 209, 73)', color: '#fff' }}
         icon={<WorkIcon />}
         visible={true}
 
       >
-        <h3 className="vertical-timeline-element-title text-green-900 text-xl">Baccaleauréat Général</h3>
-        <h4 className="vertical-timeline-element-subtitle">Option Numérique et Sciences informatiques et mathématiques</h4>
+        <h3 className="vertical-timeline-element-title text-green-900 text-xl">Enseignant et animateur</h3>
+        <h4 className="vertical-timeline-element-subtitle">Association <a href="https://www.helloasso.com/associations/apas-bethunois" target="_blank" className="text-blue-700">APAS</a></h4>
         <p>
-          Je continue ma formation dans l&apos;informatique en choisissant l&apos;option <b>NSI</b> au <a href="https://www.lyceecarnot.fr" target="_blank" className="text-blue-700">lycée</a>. J&apos;obtiens mon <b>baccaleauréat</b> avec mention <b>bien</b>. Ayant précédemment appris les bases de l&apos;informatique, je trouve les cours simples et intéressants.
+          Dès Octobre 2023, je deviens <b>animateur</b> et <b>enseignant</b> dans l&apos;association <b>APAS</b>. Je donne plus de <b>10 heures</b> de cours par semaine aux enfants de tout âge. Je découvre ce monde qui m&apos;était nouveau et me <b>passionne</b> pour <b>l&apos;enseignement</b>. Les enfants sont <b>attentionnés</b> et <b>curieux</b>, ce qui me pousse à continuer dans cette voie.
         </p>
       </VerticalTimelineElement>
-      <VerticalTimelineElement
-        className="vertical-timeline-element--work"
-        date="2021 - 2024"
-        iconStyle={{ background: 'rgb(126, 200, 227)', color: '#fff' }}
-        icon={<WorkIcon />}
-        visible={true}
 
-      >
-        <h3 className="vertical-timeline-element-title text-green-900 text-xl">B.U.T Informatique</h3>
-        <h4 className="vertical-timeline-element-subtitle">Bachelor Universitaire de Technologie</h4>
-        <p>
-          C&apos;est donc naturellement que je m&apos;oriente vers un <b><a href="http://www.iut-lens.univ-artois.fr/formations/but-informatique/" target="_blank" className="text-blue-700">B.U.T Informatique</a></b> à l&apos;I.U.T. de Lens J&apos;y réapprends les bases de l&apos;informatique et me spécialise dans le <b>développement Web</b>. Je découvre également le <b>monde professionnel</b> durant deux stages...
-        </p>
-      </VerticalTimelineElement>
 
       <VerticalTimelineElement
         className="vertical-timeline-element--education"
@@ -103,45 +106,57 @@ const Experience = () => {
 
       <VerticalTimelineElement
         className="vertical-timeline-element--work"
-        date="2023 - 2024"
-        iconStyle={{ background: 'rgb(162, 209, 73)', color: '#fff' }}
+        date="2021 - 2024"
+        iconStyle={{ background: 'rgb(126, 200, 227)', color: '#fff' }}
         icon={<WorkIcon />}
         visible={true}
 
       >
-        <h3 className="vertical-timeline-element-title text-green-900 text-xl">Enseignant et animateur</h3>
-        <h4 className="vertical-timeline-element-subtitle">Association <a href="https://www.helloasso.com/associations/apas-bethunois" target="_blank" className="text-blue-700">APAS</a></h4>
+        <h3 className="vertical-timeline-element-title text-green-900 text-xl">B.U.T Informatique</h3>
+        <h4 className="vertical-timeline-element-subtitle">Bachelor Universitaire de Technologie</h4>
         <p>
-          Dès Octobre 2023, je deviens <b>animateur</b> et <b>enseignant</b> dans l&apos;association <b>APAS</b>. Je donne plus de <b>10 heures</b> de cours par semaine aux enfants de tout âge. Je découvre ce monde qui m&apos;était nouveau et me <b>passionne</b> pour <b>l&apos;enseignement</b>. Les enfants sont <b>attentionnés</b> et <b>curieux</b>, ce qui me pousse à continuer dans cette voie.
+          C&apos;est donc naturellement que je m&apos;oriente vers un <b><a href="http://www.iut-lens.univ-artois.fr/formations/but-informatique/" target="_blank" className="text-blue-700">B.U.T Informatique</a></b> à l&apos;I.U.T. de Lens J&apos;y réapprends les bases de l&apos;informatique et me spécialise dans le <b>développement Web</b>. Je découvre également le <b>monde professionnel</b> durant deux stages...
         </p>
       </VerticalTimelineElement>
+
       <VerticalTimelineElement
-        className="vertical-timeline-element--education"
-        date="2024"
-        iconStyle={{ background: 'rgb(162, 209, 73)', color: '#fff' }}
+        className="vertical-timeline-element--work"
+        date="2020 - 2021"
+        iconStyle={{ background: 'rgb(126, 200, 227)', color: '#fff' }}
+        icon={<WorkIcon />}
         visible={true}
 
       >
-        <h3 className="vertical-timeline-element-title text-green-900 text-xl">Diplôme d&apos;Animateur Fédéral</h3>
-        <h4 className="vertical-timeline-element-subtitle">Karaté</h4>
+        <h3 className="vertical-timeline-element-title text-green-900 text-xl">Baccaleauréat Général</h3>
+        <h4 className="vertical-timeline-element-subtitle">Option Numérique et Sciences informatiques et mathématiques</h4>
         <p>
-          A côté de mes études, je continue après <b>10 ans</b> de pratique, à m&apos;investir dans le <b>karaté</b>. Je suis en train de passer mon <b><a href="https://www.ffkarate.fr/espace-licencies/formations/diplome-animateur-federal-daf/" target="_blank" className="text-blue-700" >diplôme d&apos;animateur fédéral</a></b> tout en <b>enseignant</b> avec le professeur dans mon club. 
+          Je continue ma formation dans l&apos;informatique en choisissant l&apos;option <b>NSI</b> au <a href="https://www.lyceecarnot.fr" target="_blank" className="text-blue-700">lycée</a>. J&apos;obtiens mon <b>baccaleauréat</b> avec mention <b>bien</b>. Ayant précédemment appris les bases de l&apos;informatique, je trouve les cours simples et intéressants.
         </p>
       </VerticalTimelineElement>
+
+
+        <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            date="2016 - 2018"
+            iconStyle={{ background: 'rgb(162, 209, 73)', color: '#fff' }}
+            icon={<WorkIcon />}
+            visible={true}
+                    >
+            <h3 className="vertical-timeline-element-title text-green-900 text-xl">Auto apprentissage de l&apos;informatique</h3>
+            <h4 className="vertical-timeline-element-subtitle ">Les débuts en autodidacte</h4>
+            <p>
+            Je découvre l&apos;informatique et <b>l&apos;auto apprentissage</b> sur internet durant mes années de <b>collège et de lycée</b>. J&apos;apprends notamment sur <em>le site du 0</em> qui devient ensuite <a href="https://openclassrooms.com/fr/" target="_blank" className="text-blue-700">openclassroom</a>.
+            </p>
+        </VerticalTimelineElement>
       
-      <VerticalTimelineElement
-        className="vertical-timeline-element--education"
-        date="Aujourd'hui"
-        iconStyle={{ background: 'rgb(162, 209, 73)', color: '#fff' }}
-        visible={true}
+      
 
-      >
-        <h3 className="vertical-timeline-element-title text-green-900 text-xl">Candidature à la Licence 3 en éducation</h3>
-        <h4 className="vertical-timeline-element-subtitle">Licence éducation à distance</h4>
-        <p>
-          Suite à mon <b>parcours formateur</b> dans l&apos;enseignement, je souhaite continuer dans cette voie. Je postule donc pour une <b>licence 3 en éducation</b> à distance. J&apos;espère ainsi continuer à <b>m&apos;investir</b> dans l&apos;éducation et à aider les enfants à grandir. Au-délà de ma passion pour l&apos;enseignement, c&apos;est également les méthodes d&apos;apprentissage qui m&apos;intéressent. Curieux et ambitieux, j&apos;aime <b>comprendre</b> comment la société évolue afin d&apos;être un acteur de son amélioration.
-        </p>
-      </VerticalTimelineElement>
+      
+
+      
+      
+      
+      
       <VerticalTimelineElement
         iconStyle={{ background: 'rgb(16, 204, 82)', color: '#fff' }}
         visible={true}
--- app/valeurs/page.tsx
+++ app/valeurs/page.tsx
@@ -35,18 +35,23 @@ const Valeurs = () => {
             <section className="bg-mountain py-12">
         <div className="container mx-auto px-4">
           <h2 className="text-3xl font-bold mb-6 text-primary text-center">Quelles sont mes valeurs ?</h2>
-          <p className="text-lg text-gray-300 mb-8">
+          <p className="text-lg text-gray-300 mb-8 text-justify">
             Je souhaite à travers la licence Education devenir un <b>éducateur accompli</b>. Mon objectif est de contribuer <b>positivement</b> à la société en formant les futures générations. Je crois fermement en une approche éducative centrée sur l&apos;apprenant, qui valorise l&apos;autonomie, la créativité et l&apos;inclusion.
           </p>
-          <p className="text-lg text-gray-300 mb-8">
+          <p className="text-lg text-gray-300 mb-8 text-justify">
             Mon expérience en tant qu&apos;animateur et enseignant dans l&apos;association <a href="https://www.helloasso.com/associations/apas-bethunois" target="_blank" className="text-blue-700">APAS</a> m&apos;a permis de renforcer ma conviction en l&apos;importance de l&apos;éducation. J&apos;ai en effet la <b>conviction</b> que l&apos;éducation n&apos;est pas seulement un <b>levier</b> pour l&apos;épanouissement personnel et le développement de l&apos;individu, mais c&apos;est <b>le</b> levier pour le développement de la société.
           </p>
-          <p className="text-lg text-gray-300 mb-8">
+          <p className="text-lg text-gray-300 mb-8 text-justify">
             Mes ambitions sont donc multiples à travers ce parcours d&apos;enseignant, je ne souhaite pas seulement transmettre des connaissances. Je souhaite concevoir et collaborer dans des projets éducatifs qui améliorent la qualité de l&apos;éducation de tous.
           </p>
+
+
+          
+
+
           <section className="py-12 bg-opacity-25">
               <h2 className="text-3xl font-bold mb-6 text-center text-primary">Mes Projets</h2>
-              <p className="text-lg text-gray-300 mb-8 text-center">
+              <p className="text-lg text-gray-300 mb-8 text-justify ">
                   Voici quelques-uns des projets éducatifs sur lesquels je travaille ou envisage, chacun visant à apporter une contribution significative à l&apos;éducation et à l&apos;apprentissage. Pour leur accomplissement de la meilleure des manières, rien de mieux que de <b>compléter</b> mon parcours avec une formation dans l&apos;éducation qui me permettrait d&apos;avoir les <b>compétences</b> nécessaires pour mener à bien ces projets.
               </p>
               {/* Conteneur des projets ici */}
